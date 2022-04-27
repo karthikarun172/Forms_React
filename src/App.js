e19c6411f2002c7b46ac29f5bd8a8ff00b78586d
arun <karthikarun172@gmail.com>
2022-04-27 12:39:17 +0530

    Multiple Form Input

--- src/App.js
+++ src/App.js
@@ -5,24 +5,51 @@ import { useState } from 'react';
 function App() {
   const [Email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const [checked, setChecked] = useState(false)
+  const [calender, setCalender] = useState("")
+  const [selected, setSelected] = useState("")
+  const [range, setRange] = useState(0)
+
 
   const data = {
     email_Data: Email,
-    Password_Data: password
+    Password_Data: password,
+    Checked_Data: checked,
+    Calender_Data: calender,
+    selected_Data: selected,
+    Range_Data: range
+
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(data)
+
   }
 
 
-  console.log("done - ");
   return (
-    <div className="App">
+    <div className="mainContainer"  >
 
-      <form onSubmit={(e) => {
-        e.preventDefault();
-        console.log(data)
-      }} >
+      <form
+        className='FormContainer'
+        onSubmit={handleSubmit} >
         <h2>Login Form</h2>
-        <input placeholder='Email' onChange={e => setEmail(e.target.value)}  ></input>
-        <input placeholder='Password' type="password" onChange={e => setPassword(e.target.value)}  ></input>
+        <input placeholder='Email' onChange={e => setEmail(e.target.value)} value={Email} />
+        <input placeholder='Password' type="password" onChange={e => setPassword(e.target.value)} value={password} />
+        <input type="month" onChange={e => setCalender(e.target.value)} />
+        <input type="range" onChange={e => setRange(e.target.value)} min="0" max="100" />
+
+
+        <select value={selected} onChange={e => setSelected(e.target.value)}>
+          <option value="None">None</option>
+          <option value="grapefruit">Grapefruit</option>
+          <option value="lime">Lime</option>
+          <option value="coconut">Coconut</option>
+          <option value="mango">Mango</option>
+        </select>
+        <input type="checkbox" onChange={(e) => setChecked(!checked)} />
+
         <button type='submit' >login</button>
       </form>
     </div>
