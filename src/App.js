769061243eba7a5a7c1a1d5ee9b61266bcc93273
arun <karthikarun172@gmail.com>
2022-04-27 12:00:07 +0530

    Basic Form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [Email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const data = {
+    email_Data: Email,
+    Password_Data: password
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        console.log(data)
+      }} >
+        <h2>Login Form</h2>
+        <input placeholder='Email' onChange={e => setEmail(e.target.value)}  ></input>
+        <input placeholder='Password' type="password" onChange={e => setPassword(e.target.value)}  ></input>
+        <button type='submit' >login</button>
+      </form>
     </div>
   );
 }
